9e5fccff1b09abed46ddc8ae8ffc9dcff5ab3663
Thomas Durand <durandthomas@protonmail.com>
2024-09-26 18:49:18 +0200

    fix - added value to multiselect

--- src/components/features/job-search/multi-select.tsx
+++ src/components/features/job-search/multi-select.tsx
@@ -9,59 +9,59 @@ import {
 import * as React from 'react';
 
 interface MutliSelectProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  value: string[];
   enumValues: string[];
   label: string;
 }
 
-const MultiSelect = ({
-  enumValues,
-  className,
-  label,
-  ...props
-}: MutliSelectProps) => {
-  const [selectedValues, setSelectedValues] = React.useState<string[]>([]);
+const MultiSelect = React.forwardRef<HTMLInputElement, MutliSelectProps>(
+  ({ enumValues, className, label, value, ...props }, ref) => {
+    const [selectedValues, setSelectedValues] = React.useState<string[]>(value);
 
-  const handleCheckedChange = (value: string) => {
-    setSelectedValues((prevValues) =>
-      prevValues.includes(value)
-        ? prevValues.filter((v) => v !== value)
-        : [...prevValues, value]
-    );
-  };
-  return (
-    <Popover>
-      <PopoverTrigger asChild>
-        <Button
-          variant='outline'
-          className='text-muted-foreground'
-        >
-          {label}
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent className='space-y-4'>
-        {enumValues.map((value) => (
-          <div
-            key={value}
-            className='flex gap-2 items-center'
+    const handleCheckedChange = (value: string) => {
+      setSelectedValues((prevValues) =>
+        prevValues.includes(value)
+          ? prevValues.filter((v) => v !== value)
+          : [...prevValues, value]
+      );
+    };
+    return (
+      <Popover>
+        <PopoverTrigger asChild>
+          <Button
+            variant='outline'
+            className='text-muted-foreground'
           >
-            <Checkbox
-              id={value}
-              name='contract_type'
-              value={value}
-              checked={selectedValues.includes(value)}
-              onCheckedChange={() => handleCheckedChange(value)}
-            />
-            <Label
-              htmlFor='contract_type'
-              className='capitalize'
+            {label}
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className='space-y-4'>
+          {enumValues.map((value) => (
+            <div
+              key={value}
+              className='flex gap-2 items-center'
             >
-              {value}
-            </Label>
-          </div>
-        ))}
-      </PopoverContent>
-    </Popover>
-  );
-};
+              <Checkbox
+                id={value}
+                name='contract_type'
+                value={value}
+                checked={selectedValues.includes(value)}
+                onCheckedChange={() => handleCheckedChange(value)}
+              />
+              <Label
+                htmlFor='contract_type'
+                className='capitalize'
+              >
+                {value}
+              </Label>
+            </div>
+          ))}
+        </PopoverContent>
+      </Popover>
+    );
+  }
+);
+
+MultiSelect.displayName = 'MultiSelect';
 
 export default MultiSelect;
