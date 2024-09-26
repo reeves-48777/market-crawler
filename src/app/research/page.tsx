283d3d519901bb44b4793fdc4a4e7f46be18a88f
Thomas Durand <durandthomas@protonmail.com>
2024-09-26 17:49:19 +0200

    feat - added research page and layout

---
+++ src/app/research/page.tsx
@@ -0,0 +1,9 @@
+import ResearchTester from '@/components/features/job-search/research-tester';
+
+export default function ResearchPage() {
+  return (
+    <div className='flex flex-col w-full items-center justify-center gap-4'>
+      <ResearchTester />
+    </div>
+  );
+}
