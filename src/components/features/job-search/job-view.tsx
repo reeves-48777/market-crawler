8ad49d882eb65739d48adfd1892dcfe81a864b95
Thomas Durand <durandthomas@protonmail.com>
2024-09-26 17:47:57 +0200

    feat - added job view card

---
+++ src/components/features/job-search/job-view.tsx
@@ -0,0 +1,59 @@
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from '@/components/ui/card';
+import { Clock, Laptop, PiggyBank, Signature } from 'lucide-react';
+
+type JobViewProps = {
+  job: any;
+};
+
+export default function JobView({ job }: JobViewProps) {
+  const contractType = JSON.parse(job.contractType);
+  const remoteMode = JSON.parse(job.remoteMode);
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>{job.title}</CardTitle>
+        <CardDescription className='flex flex-row justify-between'>
+          <em>{`published at ${new Date(
+            job.publicationDate
+          ).toLocaleDateString()}`}</em>
+          <strong>{JSON.parse(job.company)}</strong>
+        </CardDescription>
+      </CardHeader>
+      <CardContent className='grid grid-rows-2 grid-cols-2 gap-2'>
+        <div className='flex flex-row gap-3 items-center'>
+          <PiggyBank />
+          <span>
+            {job.annualIncome
+              ? `${job.annualIncome} / year`
+              : `${job.dailyIncome} / day`}
+          </span>
+        </div>
+        <div className='flex flex-row gap-3 items-center'>
+          <Clock />
+          <span>{`${job.duration} month(s)`}</span>
+        </div>
+        <div className='flex flex-row gap-3 items-center'>
+          <Laptop />
+          <span>
+            {Array.isArray(remoteMode) ? remoteMode.join(', ') : remoteMode}
+          </span>
+        </div>
+        <div className='flex flex-row gap-3 items-center'>
+          <Signature />
+          <span>
+            {Array.isArray(contractType)
+              ? contractType.join(', ')
+              : contractType}
+          </span>
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
