1efb8407389db11fbb98ef3df20ccbf4c11f93b6
Thomas Durand <durandthomas@protonmail.com>
2024-09-26 17:46:03 +0200

    feat - added dummy dashboard / main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,147 @@
-import Image from "next/image";
+import { auth } from '@/lib/auth';
+import { Card } from '@/components/ui/card';
+import ChartDonut from '@/components/features/charts/donut';
+import ChartRadial from '@/components/features/charts/radial';
+import ChartRadar from '@/components/features/charts/radar';
+import ChartLine from '@/components/features/charts/line';
+import ChartBar from '@/components/features/charts/bar';
+import { Button, buttonVariants } from '@/components/ui/button';
+import { PlusCircle } from 'lucide-react';
+import { Avatar, AvatarFallback } from '@/components/ui/avatar';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@/components/ui/tooltip';
+import ChartArea from '@/components/features/charts/area';
+import { cn } from '@/lib/utils';
+import Link from 'next/link';
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+export default async function Home() {
+  const session = await auth();
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const sideOffset = 32;
+
+  if (session && session.user) {
+    return (
+      <main className='flex flex-col h-full w-full items-start pl-32 pt-16 pb-12 pr-12 overflow-y-auto'>
+        <h1 className='text-4xl font-semibold'>
+          Welcome {session.user.name?.split(' ').at(0)} !
+        </h1>
+        <h2 className='text-xl'>
+          Here are the results from your previous researches
+        </h2>
+        <div className='flex w-full h-full mt-12 gap-64'>
+          <div className='grid w-full h-full grid-cols-3 2xl:grid-cols-4 grid-rows-2 gap-12'>
+            <ChartRadial className='col-start-1 row-start-1' />
+            <ChartDonut className='col-start-1 row-start-2' />
+            <ChartRadar className='max-2xl:hidden col-start-2 row-start-2' />
+            <ChartArea className='col-start-2 2xl:col-start-3 row-start-2' />
+            <ChartLine className='col-start-2 row-start-1 col-span-2' />
+            <ChartBar className='max-2xl:hidden col-start-4 row-start-1 row-span-2' />
+          </div>
+          <Card className='w-20 h-full flex flex-col items-center justify-start gap-8 py-4'>
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <Link
+                    href='/research'
+                    className={cn(
+                      buttonVariants({ variant: 'secondary', size: 'icon' }),
+                      'rounded-full size-12'
+                    )}
+                  >
+                    <PlusCircle />
+                  </Link>
+                </TooltipTrigger>
+                <TooltipContent
+                  side='left'
+                  sideOffset={sideOffset}
+                  className='text-xl'
+                >
+                  New research
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <Button
+                    variant='outline'
+                    size='icon'
+                    className='rounded-full'
+                  >
+                    <Avatar>
+                      <AvatarFallback className='bg-transparent'>
+                        R
+                      </AvatarFallback>
+                    </Avatar>
+                  </Button>
+                </TooltipTrigger>
+                <TooltipContent
+                  side='left'
+                  sideOffset={sideOffset}
+                  className='text-xl bg-muted text-primary'
+                >
+                  React developer
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <Button
+                    variant='outline'
+                    size='icon'
+                    className='rounded-full'
+                  >
+                    <Avatar>
+                      <AvatarFallback className='bg-transparent'>
+                        N
+                      </AvatarFallback>
+                    </Avatar>
+                  </Button>
+                </TooltipTrigger>
+                <TooltipContent
+                  side='left'
+                  sideOffset={sideOffset}
+                  className='text-xl bg-muted text-primary'
+                >
+                  NextJS developer
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <Button
+                    variant='outline'
+                    size='icon'
+                    className='rounded-full'
+                  >
+                    <Avatar>
+                      <AvatarFallback className='bg-transparent'>
+                        T
+                      </AvatarFallback>
+                    </Avatar>
+                  </Button>
+                </TooltipTrigger>
+                <TooltipContent
+                  side='left'
+                  sideOffset={40}
+                  className='text-xl bg-muted text-primary'
+                >
+                  ThreeJS developer
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+          </Card>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    );
+  }
+  return (
+    <main className='flex h-full w-full items-center justify-center'></main>
   );
 }
