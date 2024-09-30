751aa86e40089b4e7092b757e5b98ae39c4648be
Thomas Durand <durandthomas@protonmail.com>
2024-09-30 13:08:08 +0200

    feat - added blocking on account page

---
+++ src/app/account/layout.tsx
@@ -0,0 +1,5 @@
+const AccountSettingsLayout = ({ children }: PropsWithChildren) => {
+  return <div className='flex flex-col w-full h-full p-24'>{children}</div>;
+};
+
+export default AccountSettingsLayout;
--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,47 +1,81 @@
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
-import { Card, CardContent, CardHeader } from '@/components/ui/card';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
 import { auth } from '@/lib/auth';
 
 export default async function AccountPage() {
   const session = await auth();
 
   return (
-    <div className='grid grid-cols-2 grid-rows-2 p-16 items-center gap-8 w-full h-4/5'>
-      <Card className='row-span-2'>
-        <CardHeader className='flex flex-row gap-4 items-center justify-center'>
-          <Avatar>
-            <AvatarFallback>{session?.user?.name?.charAt(0)}</AvatarFallback>
+    <div className='grid grid-cols-2 grid-rows-2 gap-8'>
+      <Card>
+        <CardHeader className='gap-4'>
+          <CardTitle>Your profile</CardTitle>
+          <Avatar className='mx-auto'>
+            <AvatarFallback>{session?.user?.name?.[0]}</AvatarFallback>
             <AvatarImage
               src={session?.user?.image}
               alt='account image'
             />
           </Avatar>
-          <div className='flex flex-col'>
-            <span>{session?.user?.name}</span>
-            <span>{session?.user?.email}</span>
-          </div>
         </CardHeader>
-        <CardContent className='flex flex-col gap-4'>
-          <Label>
-            Name
-            <Input
-              type='text'
-              label='name'
-            />
-          </Label>
-          <Label>
-            Email
-            <Input
-              type='text'
-              label='email'
-            />
-          </Label>
+        <CardContent className='flex flex-col gap-8'>
+          <Input
+            value={session?.user?.name}
+            label='Name'
+          />
+          <Input
+            value={session?.user?.email}
+            label='Email'
+          />
         </CardContent>
       </Card>
-      <Card />
-      <Card />
+      <Card className='border-none bg-muted/20'>
+        <CardHeader>
+          <CardTitle>Your linked accounts</CardTitle>
+        </CardHeader>
+        <CardContent className='flex flex-col gap-4'>
+          <Button
+            variant='outline'
+            size='lg'
+          >
+            Freework
+          </Button>
+          <Button
+            variant='outline'
+            size='lg'
+          >
+            HelloWork
+          </Button>
+          <Button
+            variant='outline'
+            size='lg'
+          >
+            Linkedin
+          </Button>
+          <Button size='lg'>Add account</Button>
+        </CardContent>
+      </Card>
+      <Card className='col-span-2'>
+        <CardHeader>
+          <CardTitle>Source websites</CardTitle>
+        </CardHeader>
+        <CardContent className='grid grid-cols-4 gap-6'>
+          <Card className='border-dashed'>
+            <CardContent>Free work</CardContent>
+          </Card>
+          <Card className='border-dashed'>
+            <CardContent>Hello Work</CardContent>
+          </Card>
+          <Card className='border-dashed'>
+            <CardContent>Welcome to the Jungle</CardContent>
+          </Card>
+          <Card className='border-dashed'>
+            <CardContent>Indeed</CardContent>
+          </Card>
+        </CardContent>
+      </Card>
     </div>
   );
 }
